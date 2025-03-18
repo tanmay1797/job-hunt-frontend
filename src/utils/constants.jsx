b92d1ebda32e5f2d1ba6e88ee1ecb76b2bcd808c
Tanmay <tanmaygautam1797@gmail.com>
2025-03-18 12:56:55 +0530

    made changes in the constants file

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,5 +1,12 @@
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
+// export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
+export const USER_API_END_POINT =
+  "https://job-hunt-backend-g8o1.onrender.com/api/v1/user";
+// export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
+export const JOB_API_END_POINT =
+  "https://job-hunt-backend-g8o1.onrender.com/api/v1/job";
+// export const APPLICATION_API_END_POINT ="http://localhost:8000/api/v1/application";
 export const APPLICATION_API_END_POINT =
-  "http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
+  "https://job-hunt-backend-g8o1.onrender.com/api/v1/application";
+// export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
+export const COMPANY_API_END_POINT =
+  "https://job-hunt-backend-g8o1.onrender.com/api/v1/company";
