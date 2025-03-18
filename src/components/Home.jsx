ea500596d59a4548b1a8dee4107eb2e181439b2b
Tanmay <tanmaygautam1797@gmail.com>
2025-03-18 12:46:57 +0530

    Initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import NavBar from "./ui/shared/NavBar";
+import HeroSection from "./HeroSection";
+import CategoryCarousel from "./CategoryCarousel";
+import LatestJobs from "./LatestJobs";
+import Footer from "./ui/shared/Footer";
+import useGetAllJobs from "@/hooks/useGetAllJobs";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+const Home = () => {
+  useGetAllJobs();
+  const { user } = useSelector((store) => store.auth);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (user?.role === "recruiter") {
+      navigate("/admin/companies");
+    }
+  }, []);
+  return (
+    <div>
+      <NavBar />
+      <HeroSection />
+      <CategoryCarousel />
+      <LatestJobs />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/admin/Applicants.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from "react";
+import NavBar from "../ui/shared/NavBar";
+import ApplicantsTable from "./ApplicantsTable";
+import { APPLICATION_API_END_POINT } from "@/utils/constants";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { setAllApplicants } from "@/redux/applicationSlice";
+
+const Applicants = () => {
+  const params = useParams();
+  const dispatch = useDispatch();
+  const { applicants } = useSelector((store) => store.application);
+  useEffect(() => {
+    const fetchAllApplicants = async () => {
+      try {
+        const res = await axios.get(
+          `${APPLICATION_API_END_POINT}/${params.id}/applicants`,
+          { withCredentials: true }
+        );
+        console.log(res.data);
+        dispatch(setAllApplicants(res.data.job));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchAllApplicants();
+  }, []);
+
+  return (
+    <div>
+      <NavBar />
+      <div className="max-w-7xl mx-auto">
+        <h1 className="font-bold text-xl my-5">
+          Applicants {applicants?.applications?.length}
+        </h1>
+        <ApplicantsTable />
+      </div>
+    </div>
+  );
+};
+
+export default Applicants;
